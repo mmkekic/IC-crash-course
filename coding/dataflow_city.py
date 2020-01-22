c0d615a48c397521d0241017ff607a5663ed53b6
Gonzalo <gonzaponte@gmail.com>
2020-01-22 22:24:31 +0200

    Add solution to dataflow city

--- coding/dataflow_city.py
+++ coding/dataflow_city.py
@@ -31,8 +31,66 @@ np.random.seed(314159265)
 ##############################################################################
 
 
+def generate_poisson():
+    for _ in range(10000):
+        yield dict(poisson=np.random.poisson(50))
+
+def polynom(x):
+    return x**3 + x**2 +  x
+
+def divisible_by_7(x):
+    return x % 7 == 0
+
+def divisible_by_3(x):
+    return x % 3 == 0
+
+def not_divisible(x, y):
+    return not divisible_by_7(x) and not divisible_by_3(y)
+
+@np.vectorize
+def digit_adder(x):
+    return sum(map(int, str(x)))
+
+def file_writer(file):
+    def write(data):
+        file.write(f"{data}\n")
+    return write
 
 
+count_in = fl.spy_count()
+count_sl = fl.spy_count()
+count_7  = fl.spy_count()
+count_3  = fl.spy_count()
+count_u  = fl.spy_count()
+
+compute_polynom = fl.map(    polynom, args="poisson", out= "polynom")
+sum_digits      = fl.map(digit_adder, args="polynom", out="digitsum")
+
+take_divisible_by_7 = fl.filter(divisible_by_7, args= "polynom")
+take_divisible_by_3 = fl.filter(divisible_by_3, args="digitsum")
+take_not_divisible  = fl.filter(not_divisible , args=("polynom", "digitsum"))
+
+with open("divisible_by_7.txt", "w") as file7,\
+     open("divisible_by_3.txt", "w") as file3,\
+     open(       "useless.txt", "w") as fileu:
+    write_7  = fl.sink(file_writer(file7), args="poisson")
+    write_3  = fl.sink(file_writer(file3), args="polynom")
+    write_u  = fl.sink(file_writer(fileu), args="poisson")
+
+    result = fl.push(source = generate_poisson(),
+                     pipe   = fl.pipe(count_in.spy,
+                                      fl.slice(5, None, 3),
+                                      count_sl.spy,
+                                      compute_polynom,
+                                      sum_digits,
+                                      fl.fork((take_divisible_by_7, count_7.spy, write_7),
+                                              (take_divisible_by_3, count_3.spy, write_3),
+                                              (take_not_divisible , count_u.spy, write_u))),
+                     result = dict(n_in = count_in.future,
+                                   n_sl = count_sl.future,
+                                   n_7  = count_7 .future,
+                                   n_3  = count_3 .future,
+                                   n_u  = count_u .future))
 
 ##############################################################################
 # Tests
